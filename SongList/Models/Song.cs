e3808471271cdc7a997cc99ef9166698804da35b
mhdhfz <mhd.hfzz@gmail.com>
2021-08-22 07:48:13 +0800

    add song model and give attribute

---
+++ SongList/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongList.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+
+        [Required(ErrorMessage = "Please enter song name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year")]
+        [Range(1900, 2021, ErrorMessage = "Year must between 1900 and 2021")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter the rating")]
+        [Range(1900, 2021, ErrorMessage = "Rating must between 1 and 5")]
+        public int? Rating { get; set; }
+
+
+    }
+}
