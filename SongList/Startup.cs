abe91edcf20c01df087e2eb19e945267fe689942
mhdhfz <mhd.hfzz@gmail.com>
2021-08-22 08:13:58 +0800

    adding connection string

--- SongList/Startup.cs
+++ SongList/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SongList.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,8 @@ namespace SongList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SongContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SongConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
